c8d332b37e8f40cbd12ca4a845485c99c40ac976
James Clause <clause@udel.edu>
2021-10-20 12:02:43 -0400

    Updates for 10/19

--- hawaiian.py
+++ hawaiian.py
@@ -1,7 +1,110 @@
-from typing import NewType, List
+from enum import Enum
+from typing import NewType, List, Optional
 
 HawaiianWord = NewType('HawaiianWord', str)
 HawaiianPhrase = NewType('HawaiianPhrase', List[HawaiianWord])
+
+
+class HawaiianVowel(Enum):
+    A = "a"
+    E = "e"
+    I = "i"
+    O = "o"
+    U = "u"
+
+
+class HawaiianConsonant(Enum):
+    H = "h"
+    K = "k"
+    L = "l"
+    M = "m"
+    N = "n"
+    P = "p"
+    W = "w"
+    OKINE = "'"
+
+
+class HawaiianVowelPair(Enum):
+    AE = "ae"
+    AI = "ai"
+    AO = "ao"
+    AU = "au"
+
+    EI = "ei"
+    EU = "eu"
+
+    IU = "iu"
+
+    OI = "oi"
+    OU = "ou"
+    
+    UI = "ui"
+
+
+def vowel_from(s: str) -> Optional[HawaiianVowel]:
+    """
+    Checks whether a string is a Hawaiian vowel
+    :param s: a string
+    :return: The corresponding HawaiianVowel if one exists
+             None otherwise
+    """
+    try:
+        return HawaiianVowel(s)
+    except ValueError:
+        return None
+
+
+def consonant_from(s: str) -> Optional[HawaiianConsonant]:
+    """
+    Checks whether a string is a Hawaiian consonant
+    :param s: a string
+    :return: The corresponding HawaiianConsonant if one exists
+             None otherwise
+    """
+    try:
+        return HawaiianConsonant(s)
+    except ValueError:
+        return None
+
+
+def vowel_pair_from(s: str) -> Optional[HawaiianVowelPair]:
+    """
+    Checks whether a string is a Hawaiian vowel pair
+    :param s: a string
+    :return: The corresponding HawaiianVowelPair if one exists
+             None otherwise
+    """
+    try:
+        return HawaiianVowelPair(s)
+    except ValueError:
+        return None
+
+
+def pronounce_word(word: HawaiianWord) -> str:
+    """
+    Pronounces a given Hawaiian word
+    :param word: a Hawaiian word
+    :return: pronunciation of the given Hawaiian word
+    """
+    result = ""
+
+    index = 0
+    while index < len(word):
+        character = word[index]
+        next = word[index + 1]
+        if vowel_from(character):
+            result = result + pronounce_vowel(...)
+            index += 1
+        elif vowel_pair_from(character):
+            result = result + pronounce_vowel_pair(character, next)
+            index += 2
+        elif consonant_from(character):
+            result = result + pronounce_consonant(...)
+            index += 1
+        else:
+            ...
+
+    return result
 
 
 def pronounce_phrase(phrase: HawaiianPhrase) -> str:
@@ -11,4 +114,4 @@ def pronounce_phrase(phrase: HawaiianPhrase) -> str:
     :param phrase: a Hawaiian phrase
     :return: pronunciation of the given Hawaiian phrase
     """
-    pass
+    return " ".join(pronounce_word(word) for word in phrase).capitalize()
--- test_hawaiian.py
+++ test_hawaiian.py
@@ -1,13 +1,150 @@
+from typing import Optional
+
 import pytest
 
-from hawaiian import HawaiianPhrase, HawaiianWord, pronounce_phrase
+from hawaiian import *
+
+
+@pytest.mark.parametrize("s,expected", [
+    # Hawaiian Consonants
+    ("h", None),
+    ("k", None),
+    ("l", None),
+    ("m", None),
+    ("n", None),
+    ("p", None),
+    ("w", None),
+    ("'", None),
+
+    # Hawaiian Vowels
+    ("a", HawaiianVowel.A),
+    ("e", HawaiianVowel.E),
+    ("i", HawaiianVowel.I),
+    ("o", HawaiianVowel.O),
+    ("u", HawaiianVowel.U),
+
+    # Hawaiian Vowel Pairs
+    ("ai", None),
+    ("ae", None),
+    ("ao", None),
+    ("au", None),
+    ("ei", None),
+    ("eu", None),
+    ("iu", None),
+    ("oi", None),
+    ("ou", None),
+    ("ui", None),
+
+    # Other
+    (".", None),
+    ("!", None),
+    ("?", None),
+    ("b", None),
+])
+def test_vowel_from(s: str, expected: Optional[HawaiianVowel]):
+    assert vowel_from(s) == expected
+
+
+@pytest.mark.parametrize("s,expected", [
+    # Hawaiian Consonant
+    ("h", HawaiianConsonant.H),
+    ("k", HawaiianConsonant.K),
+    ("l", HawaiianConsonant.L),
+    ("m", HawaiianConsonant.M),
+    ("n", HawaiianConsonant.N),
+    ("p", HawaiianConsonant.P),
+    ("w", HawaiianConsonant.W),
+    ("'", HawaiianConsonant.OKINE),
+
+    # Hawaiian Vowels
+    ("a", None),
+    ("e", None),
+    ("i", None),
+    ("o", None),
+    ("u", None),
+
+    # Hawaiian Vowel Pairs
+    ("ai", None),
+    ("ae", None),
+    ("ao", None),
+    ("au", None),
+    ("ei", None),
+    ("eu", None),
+    ("iu", None),
+    ("oi", None),
+    ("ou", None),
+    ("ui", None),
+
+    # Other
+    (".", None),
+    ("!", None),
+    ("?", None),
+    ("b", None),
+])
+def test_consonant_from(s: str, expected: Optional[HawaiianConsonant]):
+    assert consonant_from(s) == expected
+
+
+@pytest.mark.parametrize("s,expected", [
+    # Hawaiian Consonants
+    ("h", None),
+    ("k", None),
+    ("l", None),
+    ("m", None),
+    ("n", None),
+    ("p", None),
+    ("w", None),
+    ("'", None),
+
+    # Hawaiian Vowels
+    ("a", None),
+    ("e", None),
+    ("i", None),
+    ("o", None),
+    ("u", None),
+
+    # Hawaiian Vowel Pairs
+    ("ai", HawaiianVowelPair.AI),
+    ("ae", HawaiianVowelPair.AE),
+    ("ao", HawaiianVowelPair.AO),
+    ("au", HawaiianVowelPair.AU),
+    ("ei", HawaiianVowelPair.EI),
+    ("eu", HawaiianVowelPair.EU),
+    ("iu", HawaiianVowelPair.IU),
+    ("oi", HawaiianVowelPair.OI),
+    ("ou", HawaiianVowelPair.OU),
+    ("ui", HawaiianVowelPair.UI),
+
+    # other
+    (".", None),
+    ("!", None),
+    ("?", None),
+    ("aa", None),
+])
+def test_vowel_pair_from(s: str, expected: Optional[HawaiianVowelPair]):
+    assert vowel_pair_from(s) == expected
+
+
+@pytest.mark.parametrize("word,pronunciation", [
+    (HawaiianWord("aloha"), "ah-loh-hah"),
+    (HawaiianWord("Kakahiaka"), "kah-kah-hee-ah-kah"),
+    (HawaiianWord("Mahalo"), "mah-hah-loh"),
+    (HawaiianWord("E"), "eh"),
+    (HawaiianWord("kono"), "koh-moh"),
+    (HawaiianWord("mai"), "meye"),
+    (HawaiianWord("maui"), "mow-ee"),
+    (HawaiianWord("kane"), "kah-neh"),
+    (HawaiianWord("humuhumunukunukuapua'a"), "hoo-moo-hoo-moo-noo-koo-noo-koo-ah-poo-ah'ah")
+])
+def test_pronounce_word(word: HawaiianWord, pronunciation: str):
+    assert pronounce_word(word) == pronunciation
 
 
 @pytest.mark.parametrize("phrase,pronunciation", [
     (HawaiianPhrase([HawaiianWord("aloha")]), "Ah-loh-hah"),
     (HawaiianPhrase([HawaiianWord("Kakahiaka")]), "Kah-kah-hee-ah-kah"),
     (HawaiianPhrase([HawaiianWord("Mahalo")]), "Mah-hah-loh"),
-    (HawaiianPhrase([HawaiianWord("E komo mai")]), "Eh koh-moh meye"),
+    (HawaiianPhrase([HawaiianWord("E"), HawaiianWord("komo"), HawaiianWord("mai")]), "Eh koh-moh meye"),
     (HawaiianPhrase([HawaiianWord("maui")]), "Mow-ee"),
     (HawaiianPhrase([HawaiianWord("kane")]), "Kah-neh"),
     (HawaiianPhrase([HawaiianWord("humuhumunukunukuapua'a")]), "Hoo-moo-hoo-moo-noo-koo-noo-koo-ah-poo-ah'ah")
